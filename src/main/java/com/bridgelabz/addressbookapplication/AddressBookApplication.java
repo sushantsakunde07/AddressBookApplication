4510b97100f53886a3db6ee6c75ea079fd5b5841
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-08-29 15:02:44 +0530

    Added: Slf4j logging

--- src/main/java/com/bridgelabz/addressbookapplication/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbookapplication/AddressBookApplication.java
@@ -1,13 +1,16 @@
 package com.bridgelabz.addressbookapplication;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+        log.info("Address Book Started in {} Environment", context.getEnvironment().getProperty("environment"));
+        log.info("Address Book DB User in {}", context.getEnvironment().getProperty("spring.datasource.username"));
     }
-
 }
